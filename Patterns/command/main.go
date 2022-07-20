ba9dd710261b64d6906e6e07e01242999e47ddf2
nezhibitskiy <nezhibitskiy@yandex.ru>
2022-07-20 22:33:39 +0300

    Added command

--- Patterns/command/main.go
+++ Patterns/command/main.go
@@ -1,1 +1,67 @@
 package main
+
+import "fmt"
+
+type command interface {
+	execute()
+}
+
+// --- Device ---
+
+type device interface {
+	on()
+	off()
+}
+
+// --- Button ---
+
+type button struct {
+	command command
+}
+
+func (b *button) press() {
+	b.command.execute()
+}
+
+// -- On Command ---
+
+type onCommand struct {
+	device device
+}
+
+func (c *onCommand) execute() {
+	c.device.on()
+}
+
+// -- On Command ---
+
+type offCommand struct {
+	device device
+}
+
+func (c *offCommand) execute() {
+	c.device.off()
+}
+
+type TV struct {
+}
+
+func (t *TV) on() {
+	fmt.Println("Device turned on")
+}
+
+func (t *TV) off() {
+	fmt.Println("Device turned off")
+}
+
+func main() {
+	tv := &TV{}
+
+	onCmd := onCommand{device: tv}
+	offCmd := offCommand{device: tv}
+	onButton := &button{command: &onCmd}
+	offButton := &button{command: &offCmd}
+
+	onButton.press()
+	offButton.press()
+}
